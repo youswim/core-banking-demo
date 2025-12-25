25fa1aef1435e9fae7ebac59e29091433850ae87
youswim <youswim96@gmail.com>
2025-12-25 21:36:07 +0900

    init: tx-saga 모듈 추가

---
+++ tx-saga/src/main/java/org/example/txsaga/TxSagaApplication.java
@@ -0,0 +1,13 @@
+package org.example.txsaga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxSagaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TxSagaApplication.class, args);
+	}
+
+}
---
+++ tx-saga/src/test/java/org/example/txsaga/TxSagaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.txsaga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TxSagaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

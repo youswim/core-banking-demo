9af47f4fc4e17d7a00c24ffb6403ee0d0b662c3b
youswim <youswim96@gmail.com>
2026-01-04 19:18:07 +0900

    init: tx-common 모듈 추가
    - 다른 서브 모듈들에서 공통으로 사용하는 클래스 및 상수를 모은 모듈

---
+++ tx-common/src/test/java/org/example/txcommon/TxCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.txcommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TxCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

6e989b9b8bc4b233cfa0ae705d1ee2904febf695
youswim <youswim96@gmail.com>
2026-01-11 19:15:41 +0900

    refactor: (tx-saga-manager) 모듈 명 변경
    -  tx-saga -> tx-saga-manager

--- tx-saga-manager/src/main/java/org/example/txsaga/TxSagaManagerApplication.java
+++ tx-saga-manager/src/main/java/org/example/txsaga/TxSagaManagerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TxSagaApplication {
+public class TxSagaManagerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TxSagaApplication.class, args);
+		SpringApplication.run(TxSagaManagerApplication.class, args);
 	}
 
 }

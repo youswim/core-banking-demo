3724f8fda6446e9ef46ca45ba0bf9481beb84328
youswim <youswim96@gmail.com>
2026-01-11 20:05:28 +0900

    init: (tx-balance)

---
+++ tx-balance/src/main/java/org/example/txbalance/TxBalanceApplication.java
@@ -0,0 +1,13 @@
+package org.example.txbalance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxBalanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TxBalanceApplication.class, args);
+    }
+
+}
---
+++ tx-balance/src/test/java/org/example/txbalance/TxBalanceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.txbalance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TxBalanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

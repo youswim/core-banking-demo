1c0fada4843f4a4b4adb3a54e3f2c59f7262240a
youswim <youswim96@gmail.com>
2026-02-16 21:36:50 +0900

    init: (tx-ledger)

---
+++ tx-ledger/src/main/java/org/example/txledger/TxLedgerApplication.java
@@ -0,0 +1,13 @@
+package org.example.txledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxLedgerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TxLedgerApplication.class, args);
+    }
+
+}
---
+++ tx-ledger/src/test/java/org/example/txledger/TxLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.txledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TxLedgerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
